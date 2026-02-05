c0cbb3d68ba8e2f897f1323ba8490123c9151a4a
Naoto515 <144767814+Naoto515@users.noreply.github.com>
2026-02-05 22:47:43 +0900

    change new_screen.py
    add scrollframe and init file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from screens.top_screen import TopScreen
+from screens.new_screen import NewScreen 
 
 def show_screen(frame):
     frame.tkraise()
@@ -10,19 +11,21 @@ root.title("TOP画面")
 root.geometry("800x600")
 
 #top_frame = tk.Frame(root)
-new_screen = tk.Frame(root)
+new_screen = NewScreen(root,show_screen)
 list_screen = tk.Frame(root)
 search_screen = tk.Frame(root)
 
 # TopScreenクラスのインスタンスを作成してtop_frameに代入
 top_frame = TopScreen(root, show_screen, new_screen, list_screen, search_screen)
+new_screen.setTopFrame(top_frame)
 
 for frame in (top_frame, new_screen, list_screen, search_screen):
     frame.place(relwidth=1, relheight=1)
 
 # 新規画面表示
-tk.Label(new_screen, text="新規登録画面", font=("Arial", 14)).pack(pady=20)
-tk.Button(new_screen, text="TOPへ戻る", command=lambda: show_screen(top_frame)).pack()
+#
+# tk.Label(new_screen, text="新規登録画面", font=("Arial", 14)).pack(pady=20)
+#tk.Button(new_screen, text="TOPへ戻る", command=lambda: show_screen(top_frame)).pack()
 
 # 一覧画面表示
 tk.Label(list_screen, text="一覧表示画面", font=("Arial", 14)).pack(pady=20)
